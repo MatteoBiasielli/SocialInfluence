af7f5e5d54c28926fab848501acc49c07623ca17
emilio <emilio.capo@mail.polimi.it>
2019-07-05 23:23:51 +0200

    Experiment restructured as function call

--- ne/ts.py
+++ ne/ts.py
@@ -3,32 +3,30 @@ import numpy as np
 import csv
 
 
-# PARAMETERS
-graph_nodes = 1000
-stimulations = 10
-repetitions = 10
-experiments = 20
-from_exp = 0
-delta = 0.95
-budget = 800
-use_features = False
-save_subname = "feats" if use_features else "no_feats"
+def ts_no_edge_experiment(net_size, stimulations, delta, use_features, experiment_index):
+    # --- PARAMETERS --- #
+    repetitions = 10
+    save_subname = "feats" if use_features else "no_feats"
 
-for k in range(from_exp, experiments):
-    print("Experiment " + str(k))
-
-    if graph_nodes == 100:
+    if net_size == 100:
         true_graph = g.GraphScaleFree.create_graph100()
         est_graph = g.GraphScaleFree.create_graph100()
-    elif graph_nodes == 1000:
+        budget = 400
+    elif net_size == 1000:
         true_graph = g.GraphScaleFree.create_graph1000()
         est_graph = g.GraphScaleFree.create_graph1000()
+        budget = 800
+    else:
+        print("No net of requested size. Shutting down.")
+        return
 
     est_graph.init_estimates(estimator="ts")
 
     differences_per_repetition = []
     performance_per_repetition = []
 
+    # --- EXPERIMENT COMPUTATION --- #
+    print("Experiment Index: " + str(experiment_index))
     # Buy seed based on model
     seeds, remainder = true_graph.seeds_at_time_zero(budget)
     for i in range(repetitions):
@@ -47,16 +45,17 @@ for k in range(from_exp, experiments):
         performance_per_repetition.append(sum(true_graph.monte_carlo_sampling(1000, seeds)))
         differences_per_repetition.append(abs(np.subtract(est_graph.get_edges(), true_graph.get_edges())))
 
-    with open("results/ts_" + save_subname + "_" + str(graph_nodes) + "nodes_" + str(repetitions) + "repetitions" +
-              str(stimulations) + "stimulations_delta" + str(delta) + "__exp" + str(k) + "_differences.csv",
+    # --- STORING EXPERIMENT RESULTS --- #
+    with open("results/ts_" + save_subname + "_" + str(net_size) + "nodes_" + str(repetitions) + "repetitions" +
+              str(stimulations) + "stimulations_delta" + str(delta) + "__exp" + str(experiment_index) + "_differences.csv",
               "w") as writeFile:
         writer = csv.writer(writeFile)
         for data in differences_per_repetition:
             writer.writerow(data)
     writeFile.close()
 
-    with open("results/ts_" + save_subname + "_" + str(graph_nodes) + "nodes_" + str(repetitions) + "repetitions" +
-              str(stimulations) + "stimulations_delta" + str(delta) + "__exp" + str(k) + "_performance.csv",
+    with open("results/ts_" + save_subname + "_" + str(net_size) + "nodes_" + str(repetitions) + "repetitions" +
+              str(stimulations) + "stimulations_delta" + str(delta) + "__exp" + str(experiment_index) + "_performance.csv",
               "w") as writeFile:
         writer = csv.writer(writeFile)
         writer.writerow(performance_per_repetition)
@@ -64,3 +63,8 @@ for k in range(from_exp, experiments):
 
     print(differences_per_repetition)
     print(performance_per_repetition)
+
+
+# TEST PARAMETERS DOMAIN ---> net_size in [100, 1000], stimulations in [10, 100], delta in [0.2, 0.4, 0.6, 0.95],
+#                             use_features in [True, False]
+ts_no_edge_experiment(net_size=1000, stimulations=10, delta=0.8, use_features=False, experiment_index=0)
