a5cb73f999f63f4c8d1a9d60d0a2f962b5a746ce
emilio <emilio.capo@mail.polimi.it>
2019-07-11 13:47:12 +0200

    Plot functionalities completed

--- ne/plot.py
+++ ne/plot.py
@@ -1,32 +1,104 @@
 import seaborn as sb
+import matplotlib.pyplot as plt
+import numpy as np
 import csv
 
-PATH = "./results/"
-N_EXP = 20
+LOAD_PATH = "./results/"
+SAVE_PATH = "./plots/"
 
 
 def read_from_csv(filename):
+    data = []
 
-    with open(PATH + filename, "r") as csv_file:
+    with open(LOAD_PATH + filename, "r") as csv_file:
         csv_reader = csv.reader(csv_file)
-        data = [row for row in csv_reader]
+        for row in csv_reader:
+            if row:
+                data.append(row)
 
     csv_file.close()
+    return data
 
-    return data[:-1]
+
+def plot_experiment_differences(nodes=1000, stimulations=10, delta=0.4, n_exp=3, features=False, save=False):
+    feats_subname = "feats" if features else "no_feats"
+    plt.title("Edge Probability Errors\n(" + str(nodes) + "n | " + feats_subname + " | " + str(stimulations)
+              + "s | " + str(delta) + "d | " + str(n_exp) + "exp)")
+    plt.xlabel("Repetition")
+    plt.ylabel("Error Over Edge Probabilities")
+    experiments_set_differences = []
+
+    for i in range(n_exp):
+        data = read_from_csv("ts_" + feats_subname + "_" + str(nodes) + "nodes_10repetitions" + str(stimulations) +
+                             "stimulations_delta" + str(delta) + "__exp" + str(i) + "_differences.csv")
+        experiments_set_differences.append(data)
+
+    # Convert to float
+    for i in range(len(experiments_set_differences)):
+        for j in range(len(experiments_set_differences[0])):
+            for k in range(len(experiments_set_differences[0][0])):
+                experiments_set_differences[i][j][k] = float(experiments_set_differences[i][j][k])
+
+    # Average over experiments
+    experiments_set_differences = list(np.mean(experiments_set_differences, axis=0))
+
+    # Plot results
+    sb.boxplot(x=[i for i in range(1, 11)], y=experiments_set_differences)
+
+    if save:
+        plt.savefig(fname=SAVE_PATH + "plot_differences_" + str(nodes) + "_" + str(stimulations) + "_" + str(delta) + "_"
+                          + feats_subname + ".png")
+
+    plt.show()
 
 
-it_nodes = [100]
-it_stim = [10, 100]
-it_delta = [0.2, 0.4, 0.8, 0.95]
-it_exp = [i for i in range(N_EXP)]
+def plot_experiment_performance(nodes=100, stimulations=10, delta=0.95, n_exp=20, features=False, save=False):
+    feats_subname = "feats" if features else "no_feats"
+    plt.title("Average Number Of Activations\n(" + str(nodes) + "n | " + feats_subname + " | " + str(stimulations)
+              + "s | " + str(delta) + "d | " + str(n_exp) + "exp)")
+    plt.xlabel("Repetition")
+    plt.ylabel("Average Number of Activations")
+    experiments_set_performance = []
 
-for i in it_nodes:
-    for j in it_stim:
-        for k in it_delta:
-            for l in it_exp:
-                data = read_from_csv("ts_no_feats_" + str(i) + "nodes_10repetitions" + str(j) + "stimulations_delta" + str(k) +
-                                     "__exp" + str(l) + "_differences.csv")
+    for i in range(n_exp):
+        data = read_from_csv("ts_" + feats_subname + "_" + str(nodes) + "nodes_10repetitions" + str(stimulations) +
+                             "stimulations_delta" + str(delta) + "__exp" + str(i) + "_performance.csv")
+        experiments_set_performance += data
 
-                print(data)
+    # Convert to float
+    for i in range(len(experiments_set_performance)):
+        for j in range(len(experiments_set_performance[0])):
+            experiments_set_performance[i][j] = float(experiments_set_performance[i][j])
 
+    # Reorder in chunks per repetition
+    processed_set_performance = []
+    for i in range(len(experiments_set_performance[0])):
+        tmp_repetition = []
+        for j in range(len(experiments_set_performance)):
+            tmp_repetition.append(experiments_set_performance[j][i])
+
+        processed_set_performance.append(tmp_repetition)
+
+    # Plot results
+    if n_exp >= 5:
+        sb.boxplot(x=[i for i in range(1, 11)], y=processed_set_performance)
+    else:
+        sb.pointplot(x=np.reshape([[i]*n_exp for i in range(1, 11)], -1), y=np.reshape(processed_set_performance, -1))
+
+    if save:
+        plt.savefig(fname=SAVE_PATH + "plot_performance_" + str(nodes) + "_" + str(stimulations) + "_" + str(delta) + "_"
+                          + feats_subname + ".png")
+
+    plt.show()
+
+
+for features in [True, False]:
+    if features:
+        stim_set = [10]
+    else:
+        stim_set = [10, 100]
+
+    for stimulations in stim_set:
+        for delta in [0.2, 0.4, 0.8, 0.95]:
+            plot_experiment_differences(nodes=100, stimulations=stimulations, delta=delta, n_exp=20, features=features, save=True)
+            plot_experiment_performance(nodes=100, stimulations=stimulations, delta=delta, n_exp=20, features=features, save=True)
